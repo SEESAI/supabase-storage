d17adf7cbd38e4ec4a9c0e7552455032223c1611
Michael Copland <mcopland@sees.ai>
2025-03-31 15:53:48 +0100

    fix: artifact registry auth

--- src/http/routes/tus/lifecycle.ts
+++ src/http/routes/tus/lifecycle.ts
@@ -90,8 +90,6 @@ export function generateUrl(
   if (!req.url) {
     throw ERRORS.InvalidParameter('url')
   }
-  proto = process.env.NODE_ENV === 'production' ? 'https' : proto
-
   let basePath = path
 
   const forwardedPath = req.headers['x-forwarded-prefix']
